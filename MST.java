f2726b09df4f37f8aa9728b479cf4c8caacb2d1c
MohammedZ666 <50874919+MohammedZ666@users.noreply.github.com>
2020-03-08 13:18:35 +0600

    Update MST.java

--- MST.java
+++ MST.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  * @author 18201214
  */
 
-public class MST{
+public class MST {
 
     /**
      * @param args the command line arguments
@@ -44,24 +44,37 @@ public class MST{
        
        return 
    }
-    
+     
     public static int[] MST(int[][]G){    
          int[] parent = new int [G[0].length];
          PriorityQueue<Key> q = new PriorityQueue<>();
-        q.add(new Key(0,0));
-        
+         q.add(new Key(0,0));
+         for(int i = 1; i<G[0].length;i++){
+             q.add(new Key(i,Double.POSITIVE_INFINITY));
+         }
         while(!q.isEmpty()){
             Key u = q.remove();
             for(int v = 0; v<G[u.vertexIndex].length;v++){
                 if(G[u.vertexIndex][v]>0){
+                    
                     Key k = new Key(v,G[u.vertexIndex][v]);
-                    if(q.contains(v)){
-                        
-                    }   
-                }
+                         for (Key k1 : q) {
+                         if(k1.vertexIndex == k.vertexIndex){
+                             if(k1.compareTo(k)== 1){
+                                 q.remove(k1);
+                                 q.add(k);
+                                 parent[v] = u.vertexIndex;
+                                 break;
+                             }
+                         
+                         }
+                        }                    
+              
+              }
            }           
         }
     return parent;    
     }
-}
 
+   
+}
